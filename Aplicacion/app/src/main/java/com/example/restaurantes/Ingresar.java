424f72c984a9703f7524b69b8bd25a9e0b841769
Alejandro Garita <luisgc2009@gmail.com>
2019-04-22 20:39:58 -0600

    Ingreso a facebook
    Falta poder obtener datos del perfil del usuario

--- Aplicacion/app/src/main/java/com/example/restaurantes/Ingresar.java
+++ Aplicacion/app/src/main/java/com/example/restaurantes/Ingresar.java
@@ -9,14 +9,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
 public class Ingresar extends AppCompatActivity {
 
+    CallbackManager callbackManager;
     EditText user;
     EditText pass;
 
@@ -56,10 +67,6 @@ public class Ingresar extends AppCompatActivity {
         return false;
     }
 
-    public void onBtnIngresarFBClicked(View view){
-
-    }
-
     public void onBtnRegistrarseClicked(View view){
         Intent intent = new Intent(this, Registrar.class);
         startActivity(intent);
@@ -71,11 +78,59 @@ public class Ingresar extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar);
 
+        callbackManager = CallbackManager.Factory.create();
         user = findViewById(R.id.txtUser);
         pass = findViewById(R.id.txtPassword);
+
+        final LoginButton loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile","email","user_birthday"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String accessToken = loginResult.getAccessToken().getToken();
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        getData(object);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("login", "El usuario cancelo el ingreso");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i("login", "Ha habido un error en el ingreso");
+                Log.e("login", error.toString());
+            }
+        });
+        if(AccessToken.getCurrentAccessToken() != null){
+            Log.i("login", "Ha hecho login exitosamente.");
+        }
+    }
+
+    private void getData(JSONObject object) {
+        //Aquí se guarda la info del facebook
+        //Foto perfil, correo y fecha nacimiento.
+        try {
+            Log.i("login", object.getString("email"));
+            Log.i("login", object.getString("user_birthday"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
