870917f44ed17da6137518da83e831dadd116bdf
Unknown <faph.2009@hotmail.com>
2019-04-23 21:23:16 -0600

    Se empieza el codigo para mostrar restaurantes mapa

--- Aplicacion/app/src/main/java/com/example/restaurantes/Fragment_Mapa.java
+++ Aplicacion/app/src/main/java/com/example/restaurantes/Fragment_Mapa.java
@@ -32,6 +32,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.concurrent.ExecutionException;
+
 public class Fragment_Mapa extends Fragment {
     View rootView;
 
@@ -40,6 +48,71 @@ public class Fragment_Mapa extends Fragment {
     LocationManager locationManager;
     LocationListener locationListener;
 
+    private class DatosRestaurante{
+        private double latitud;
+        private double longitud;
+        private String Nombre;
+        private String Horario;
+        private String Telefono;
+        private String Precio;
+        private String TipoComida;
+
+        public double getLatitud() {
+            return latitud;
+        }
+
+        public double getLongitud() {
+            return longitud;
+        }
+
+        public String getNombre() {
+            return Nombre;
+        }
+
+        public String getHorario() {
+            return Horario;
+        }
+
+        public String getTelefono() {
+            return Telefono;
+        }
+
+        public String getPrecio() {
+            return Precio;
+        }
+
+        public String getTipoComida() {
+            return TipoComida;
+        }
+
+        public void setLatitud(double latitud) {
+            this.latitud = latitud;
+        }
+
+        public void setLongitud(double longitud) {
+            this.longitud = longitud;
+        }
+
+        public void setNombre(String nombre) {
+            Nombre = nombre;
+        }
+
+        public void setHorario(String horario) {
+            Horario = horario;
+        }
+
+        public void setTelefono(String telefono) {
+            Telefono = telefono;
+        }
+
+        public void setPrecio(String precio) {
+            Precio = precio;
+        }
+
+        public void setTipoComida(String tipoComida) {
+            TipoComida = tipoComida;
+        }
+    }
 
     public static Fragment_Mapa newInstance(/*TODO Recibir datos necesarios*/) {
         Fragment_Mapa fragment = new Fragment_Mapa();
@@ -80,81 +153,131 @@ public class Fragment_Mapa extends Fragment {
             @Override
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (ActivityCompat.checkSelfPermission
-                            (getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                            &&
-                            ActivityCompat.checkSelfPermission
-                                    (getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(getContext(), "Debe aceptar el permiso para ver las ubicaciones en el mapa", Toast.LENGTH_LONG).show();
-                        requestPermissions(new String[]{
-                                Manifest.permission.ACCESS_COARSE_LOCATION,
-                                Manifest.permission.ACCESS_FINE_LOCATION
-                        }, 1); // 1 is requestCode
-                        return;
-                    } else {
-                        locationListener = new LocationListener() {
-                            @Override
-                            public void onLocationChanged(Location location) {
-                                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                                googleMap.clear();
-                                googleMap.addMarker(new MarkerOptions().position(currentLocation).title("Usted está acá"));
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
-                            }
+                if (googleMap != null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ActivityCompat.checkSelfPermission
+                                (getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                                &&
+                                ActivityCompat.checkSelfPermission
+                                        (getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(getContext(), "Debe aceptar el permiso para ver las ubicaciones en el mapa", Toast.LENGTH_LONG).show();
+                            requestPermissions(new String[]{
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                                    Manifest.permission.ACCESS_FINE_LOCATION
+                            }, 1); // 1 is requestCode
+                            return;
+                        } else {
+                            locationListener = new LocationListener() {
+                                @Override
+                                public void onLocationChanged(Location location) {
+                                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                    googleMap.clear();
+                                    googleMap.addMarker(new MarkerOptions().position(currentLocation).title("Usted está acá"));
+                                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                                }
 
-                            @Override
-                            public void onStatusChanged(String s, int i, Bundle bundle) {
-                            }
+                                @Override
+                                public void onStatusChanged(String s, int i, Bundle bundle) {
+                                }
 
-                            @Override
-                            public void onProviderEnabled(String s) {
-                            }
+                                @Override
+                                public void onProviderEnabled(String s) {
+                                }
 
-                            @Override
-                            public void onProviderDisabled(String s) {
-                            }
-                        };
+                                @Override
+                                public void onProviderDisabled(String s) {
+                                }
+                            };
 
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-                        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                        googleMap.clear();
+                            googleMap.clear();
 
-                        // For showing a move to my location button
-                        googleMap.setMyLocationEnabled(true);
+                            // For showing a move to my location button
+                            googleMap.setMyLocationEnabled(true);
 
-                        LatLng location = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-                        //googleMap.addMarker(new MarkerOptions().position(location).title("Usted está acá"));
-                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-                        // For zooming automatically to the location of the marker
-                        CameraPosition cameraPosition = new CameraPosition.Builder().target(location).zoom(15).build();
-                        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                            LatLng location = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                            //googleMap.addMarker(new MarkerOptions().position(location).title("Usted está acá"));
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                            // For zooming automatically to the location of the marker
+                            CameraPosition cameraPosition = new CameraPosition.Builder().target(location).zoom(15).build();
+                            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                        }
                     }
+
+                    try {
+                        if (!AgregarMarcadoresRestaurantes(googleMap)) {
+                            Toast.makeText(getContext(), "Error al agregar los marcadores al mapa", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    //Crear marcador onTouch
+                    googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                        @Override
+                        public void onMapClick(LatLng point) {
+                            googleMap.clear();
+                            googleMap.addMarker(new MarkerOptions().position(point));
+
+                        }
+                    });
+
+                    //Poder hacer click en marcadores para nuevo restaurante o detalle de restaurante
+                    googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                        @Override
+                        public boolean onMarkerClick(Marker marker) {
+                            return clickEnMarcadorMapa(marker);
+                        }
+                    });
                 }
-                // For dropping a marker at a point on the Map
-                //LatLng sydney = new LatLng(-34, 151);
-                //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                //Crear marcador onTouch
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(LatLng point) {
-                        googleMap.clear();
-                        googleMap.addMarker(new MarkerOptions().position(point));
-
-                    }
-                });
-
-                //Poder hacer click en marcadores para nuevo restaurante o detalle de restaurante
-                googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                    @Override
-                    public boolean onMarkerClick(Marker marker) {
-                        return clickEnMarcadorMapa(marker);
-                    }
-                });
+                else
+                    Toast.makeText(getContext(), "Error no se pudo inicializar el mapa", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private boolean AgregarMarcadoresRestaurantes(GoogleMap googleMap) throws ExecutionException, InterruptedException, JSONException {
+
+        ArrayList<DatosRestaurante> markersArray = ObtenerDatosRestaurantes();
+
+        for(int i = 0 ; i < markersArray.size() ; i++) {
+            createMarker(markersArray.get(i).getLatitud(), markersArray.get(i).getLongitud(), markersArray.get(i).getNombre(), "Horario: " +markersArray.get(i).getHorario(), "BD");
+        }
+        return false;
+    }
+
+    private ArrayList<DatosRestaurante> ObtenerDatosRestaurantes() throws ExecutionException, InterruptedException, JSONException {
+        Conexion conexion = new Conexion();
+        String resultRestaurantes = conexion.execute("https://shrouded-savannah-17544.herokuapp.com/restaurants.json", "GET").get();
+        JSONArray datos = new JSONArray(resultRestaurantes);
+
+        ArrayList<DatosRestaurante> restaurantesObtenidos = new ArrayList<DatosRestaurante>();
+        DatosRestaurante restaurante;
+
+        for(int i = 0; i < datos.length(); i++){
+            JSONObject elemento = datos.getJSONObject(i);
+            restaurante=new DatosRestaurante();
+            restaurante.setNombre(elemento.getString(""));
+
+        }
+        return restaurantesObtenidos;
+    }
+
+    protected Marker createMarker(double latitude, double longitude, String title, String snippet, String tag) {
+        Marker m= googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .anchor(0.5f, 0.5f)
+                .title(title)
+                .snippet(snippet));
+        m.setTag(tag);
+        return m;
     }
 
     private boolean clickEnMarcadorMapa(final Marker marker) {
--- Aplicacion/app/src/main/java/com/example/restaurantes/Registro_Restaurante.java
+++ Aplicacion/app/src/main/java/com/example/restaurantes/Registro_Restaurante.java
@@ -2,6 +2,7 @@ package com.example.restaurantes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registro_Restaurante extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Registro_Restaurante extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_restaurante);
+    }
+
+    public void onBtnCambiarImagen(View view) {
     }
 }
