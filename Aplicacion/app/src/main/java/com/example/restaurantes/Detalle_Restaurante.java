f99dec0c803f10d35518083454441f7c8f9ee83e
Alejandro Garita <luisgc2009@gmail.com>
2019-04-23 19:05:30 -0600

    Ventana de Detalle de Restaurantes
    Se agrega la pantalla de detalle

---
+++ Aplicacion/app/src/main/java/com/example/restaurantes/Detalle_Restaurante.java
@@ -0,0 +1,13 @@
+package com.example.restaurantes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detalle_Restaurante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_restaurante);
+    }
+}
